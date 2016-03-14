fce513d4c4630760e00612d1f5321cb63af4b741
haikizoku <gnorvene@gmail.com>
2016-03-14 08:29:16 +0100

    Added files via upload

---
+++ data.php
@@ -0,0 +1,60 @@
+<?php
+$dataMenu = array(
+		'Home' => 1,
+		'about' => 1,
+		'contact' =>1
+				);
+$jumbotronTitre = array(
+	'Hello, world!',
+	'This is an example to show the potential of an offcanvas layout pattern in Bootstrap. Try some responsive-range viewport sizes to see it in action.'
+	);
+
+
+$panelTab = array(
+		'Panel heading',
+		'Some default panel content here. Nulla vitae elit libero, a pharetra augue. Aenean lacinia bibendum nulla sed consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id dolor id nibh ultricies vehicula ut id elit.' 
+	
+	);
+$TitreTableauUsers = array(
+			'#' => 1,
+			'FirstName' => 1,
+			'LastName' => 1,
+			'Username' => 1,
+			'Username' => 1
+			
+	
+);
+
+		
+$TabUsers = [
+    [1,'Mark','Otto','@mdo' ],
+    [2,'Jacob','Thorno','@fat' ],
+    [3,'Larry','The Bird','@twitter' ],
+
+];
+
+
+$ListLink = [
+			['Link 2',14],
+			['Link 3',4],
+			['Link 4',1],
+			['Link 5',12],
+			['Link 6',0],
+			['Link 7',14],
+			['Link 8',""],
+			['Link 9',""],
+			['Link 10',""],
+
+
+	];
+
+$ListLink1 = [
+			['Link 1', 14],
+			];
+
+$footer = array(
+		'&copy' => 1,
+		'2015' => 1,
+		'Company' => 1,
+		'Inc' => 1
+	);
